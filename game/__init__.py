03fa4df8495c07bb2f3f1b0246d4c9d7ec160e10
serifemrdk <serife.murduk@std.ankaramedipol.edu.tr>
2026-05-23 17:36:30 +0300

    Resolve merge conflicts

--- game/__init__.py
+++ game/__init__.py
@@ -1,1 +1,16 @@
 from .game import Game
+try:
+	from . import coin_patch
+	coin_patch.apply_patch(Game)
+except Exception:
+	pass
+try:
+	from . import collision_patch
+	collision_patch.apply_patch(Game)
+except Exception:
+	pass
+try:
+	from . import stage_reward_patch
+	stage_reward_patch.apply_patch(Game)
+except Exception:
+	pass
